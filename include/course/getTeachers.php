50e7b71b7aa81328eea894ee267f41d5a7708857
Ibrahim Ame <ame.ibrahim@yahoo.com>
2024-10-23 14:48:36 +0300

    Get Teachers List Fix #1

--- include/course/getTeachers.php
+++ include/course/getTeachers.php
@@ -1,27 +1,29 @@
 <?php
-    include "../databaseConnection.php"; 
+include "../databaseConnection.php";
 
-    $conn = OpenConnection();
+$conn = OpenConnection();
 
-    if (!$conn) {
-        die(json_encode(["error" => "Connection failed: " . mysqli_connect_error()]));
-    }
+if (!$conn) {
+    die(json_encode(["error" => "Connection failed: " . mysqli_connect_error()]));
+}else{
+    echo "hi ocam";
+}
 
-    $query = "
+$query = "
         SELECT users.id, userDetails.name, userDetails.image, users.email FROM `users`
         INNER JOIN `userDetails` ON userDetails.id = users.id
         WHERE users.role = 'teacher'
         ORDER BY userDetails.name
     ";
 
-    $result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
-    if (!$result) {
-        echo json_encode(["error" => "Query failed: " . mysqli_error($conn)]);
-        exit;
-    }
+if (!$result) {
+    echo json_encode(["error" => "Query failed: " . mysqli_error($conn)]);
+    exit;
+}
 
-    $teachers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$teachers = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    echo json_encode($teachers);
+echo json_encode($teachers);
 
