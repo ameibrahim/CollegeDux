6cf7ef611b16d3c5aefdf888b635e78cab947576
jeries-123 <119804522+jeries-123@users.noreply.github.com>
2025-01-20 13:43:07 +0200

    gpt 4o

--- js/Question.js
+++ js/Question.js
@@ -342,7 +342,7 @@ async function generateQuestion(generateQuestionObject, amount = 1) {
           Authorization: `Bearer ${apiKey}`,
         },
         body: JSON.stringify({
-          model: "gpt-4",
+          model: "gpt-4o",
           messages: [
             {
               role: "system",
